7e0a77ac7eda9eec87ad60607d6622c94a208b8e
sunlijie <sunlijie@jd.com>
2017-12-08 17:24:39 +0800

    更改目录结构  添加 request response

--- core/src/main/java/com/sunny/core/proxy/RefererInvocationHandler.java
+++ core/src/main/java/com/sunny/core/proxy/RefererInvocationHandler.java
@@ -1,5 +1,9 @@
 package com.sunny.core.proxy;
 
+import com.alibaba.fastjson.JSON;
+import com.sunny.core.rpc.DefaultRequest;
+import com.sunny.core.utils.RequestIdGenerator;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
@@ -11,8 +15,50 @@ import java.lang.reflect.Method;
  */
 
 public class RefererInvocationHandler implements InvocationHandler {
+
+    private Class cls;
+
+    public RefererInvocationHandler(Class serviceClass) {
+        this.cls = serviceClass;
+    }
+
+
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+
+        // 1. 构建request 对象
+        DefaultRequest request = new DefaultRequest();
+
+        request.setRequestId(RequestIdGenerator.getRequestId());
+        request.setArguments(args);
+        request.setMethodName(method.getName());
+        request.setInterfaceName(cls.getName());
+
+
+        //todo log
+        System.out.println(JSON.toJSONString(request));
+
+        /**
+         * 2. 序列化
+         *      1. 自定义协议头 todo
+         */
+
+        //3.发送请求
+
+
+
+
+        //4.处理结果 ，返回接口对应的返回类型
+
+        /**
+         * 5.处理异常情况
+         *      1.业务异常
+         *      2.框架异常
+         */
+
+
+
 
 
 
--- core/src/test/java/com/sunny/core/test/Test.java
+++ core/src/test/java/com/sunny/core/test/Test.java
@@ -1,5 +1,11 @@
 package com.sunny.core.test;
 
+import com.sunny.core.proxy.ProxyFactory;
+import com.sunny.core.proxy.RefererInvocationHandler;
+import com.sunny.core.proxy.impl.JdkProxyFactory;
+import com.sunny.core.service.HelloWorld;
+import com.sunny.core.service.HelloWorldImpl;
+
 /**
  * todo Description
  *
@@ -19,21 +25,31 @@ public class Test {
         this.port = port;
     }
 
-    public Test(int port) {
-        this.port = port;
-    }
+//    public Test(int port) {
+//        this.port = port;
+//    }
 
-    public void tt() {
-        Test test = new Test(6666);
+//    public void tt() {
+//        Test test = new Test(6666);
+//
+//        System.out.println(test.getPort());
+//
+//    }
 
-        System.out.println(test.getPort());
 
+    @org.junit.Test
+    public void testProxy() {
+        ProxyFactory proxyFactory = new JdkProxyFactory();
+
+        HelloWorld helloWorld = proxyFactory.getProxy(HelloWorld.class, new RefererInvocationHandler(HelloWorld.class));
+
+        helloWorld.say("sunlijie");
     }
 
     public static void main(String[] args) {
-        Test test = new Test(6666);
 
-        System.out.println(test.getPort());
+
+
     }
 
 }
